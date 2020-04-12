bf2e07574abe02f1b58652bae7876e883ed2e700
PatricioCarcani <carcani.patricio@gmail.com>
2020-04-12 14:39:57 -0300

    Added Eureka and deleted Ribbon

--- springboot-servicio-productos/src/main/java/ar/com/karkanis/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/ar/com/karkanis/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,9 @@ package ar.com.karkanis.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
