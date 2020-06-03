dc11d9772fe5c530f2631e5037765c3323def249
“Patricio <carcani.patricio@gmail.com>
2020-06-02 23:17:04 -0300

    Added Hystrix, a latency and tolerance library

--- springboot-servicio-item/src/main/java/ar/com/karkanis/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/ar/com/karkanis/springboot/app/item/controllers/ItemController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import ar.com.karkanis.springboot.app.item.models.Item;
+import ar.com.karkanis.springboot.app.item.models.Producto;
 import ar.com.karkanis.springboot.app.item.models.service.ItemService;
 
 @RestController
@@ -23,9 +26,23 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	
+	// para Hystrix
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
 	}
 
 }
