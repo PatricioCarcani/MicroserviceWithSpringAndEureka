ffdd62ecb3e4da03cf6cb9e547ac4f14f84bd70c
PatricioCarcani <carcani.patricio@gmail.com>
2020-06-02 21:32:28 -0300

    Agregado Eureka Server

---
+++ springboot-servicio-eureka-server/src/test/java/ar/com/karkanis/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.karkanis.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
